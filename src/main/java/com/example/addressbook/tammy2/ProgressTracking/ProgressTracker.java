129157b9def3a92147b99a9bda21e26420fb02c2
chrispace03 <chrispace03@gmail.com>
2024-04-29 15:17:18 +1000

    Implemented javadoc comments in ProgressTracker

--- src/main/java/com/example/addressbook/tammy2/ProgressTracking/ProgressTracker.java
+++ src/main/java/com/example/addressbook/tammy2/ProgressTracking/ProgressTracker.java
@@ -4,25 +4,42 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * Tracks the progress of a user's currency and log activities over time.
+ */
 public class ProgressTracker {
     public int currency;
     public List<String> logs;
     private final Date startDate;
 
-    // Constructor
+    /**
+     * Constructs a new ProgressTracker with an initial currency amount.
+     *
+     * @param initialCurrency the initial amount of currency the user has.
+     */
     public ProgressTracker(int initialCurrency){
         this.currency = initialCurrency;
         this.logs = new ArrayList<>();
         this.startDate = new Date(); // Set start date/time
     }
 
-    // Function to log progress and update currency
+    /**
+     * Logs a new progress entry and updates the currency based on a provided change.
+     *
+     * @param log the progress log to add.
+     * @param currencyChange the change in currency to apply.
+     */
     public void logProgress(String log, int currencyChange){
         logs.add(log);
         currency += currencyChange;
     }
 
-    // Function to get progress based on current date/time compared with start
+    /**
+     * Returns a string describing the current progress based on the time elapsed
+     * since the start date.
+     *
+     * @return a string representing the elapsed time and progress.
+     */
     public String getProgress(){
         Date currentDate = new Date();
         long elapsedTime = currentDate.getTime() - startDate.getTime();
@@ -31,14 +48,29 @@ public class ProgressTracker {
         return "Progress: " + elapsedTime + " time passed.";
     }
 
+    /**
+     * Returns the current amount of currency.
+     *
+     * @return the current currency.
+     */
     public int getCurrency() {
         return currency;
     }
 
+    /**
+     * Updates the currency by a specified amount.
+     *
+     * @param amount the amount to add or subtract from the current currency.
+     */
     public void updateCurrency(int amount) {
         currency += amount;
     }
 
+    /**
+     * Returns the list of all progress logs.
+     *
+     * @return a list of progress log entries.
+     */
     public List<String> getLogs() {
         return logs;
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.example.addressbook.tammy2 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.junit.jupiter.api;
+    requires junit;
 
 
 

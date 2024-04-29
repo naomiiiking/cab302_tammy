6f17e89b16a01d4508781a5bfccb45f1ae75f42e
chrispace03 <chrispace03@gmail.com>
2024-04-29 15:34:10 +1000

    Implemented javadoc comments in Item and Shop

--- src/main/java/com/example/addressbook/tammy2/ShopBackEnd/Item.java
+++ src/main/java/com/example/addressbook/tammy2/ShopBackEnd/Item.java
@@ -1,18 +1,37 @@
 package com.example.addressbook.tammy2.ShopBackEnd;
 
+/**
+ * Represents an item that can be purchased in the shop.
+ */
 public class Item {
     private String name;
     private int price;
 
+    /**
+     * Constructs a new Item with a name and price.
+     *
+     * @param name the name of the item.
+     * @param price the price of the item in currency units.
+     */
     public Item (String name, int price){
         this.name = name;
         this.price = price;
     }
 
+    /**
+     * Returns the name of the item.
+     *
+     * @return the name of the item.
+     */
     public String getName(){
         return name;
     }
 
+    /**
+     * Returns the price of the item.
+     *
+     * @return the price of the item in currency units.
+     */
     public int getPrice(){
         return price;
     }
